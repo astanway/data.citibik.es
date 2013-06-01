ef1e1878a666e2e35ef0dcd80c3deb69889f317e
Abe Stanway <abestanway@gmail.com>
2013-06-01 00:45:14 -0400

    Timeseries CSVs

---
+++ distill_timeseries.py
@@ -0,0 +1,21 @@
+import os
+import simplejson as json
+import msgpack
+from collections import defaultdict
+
+timeseries = defaultdict(str)
+
+for timestamp in os.listdir('data'):
+    with open('data/' + timestamp, 'r') as f:
+        data = json.loads(f.read())
+        for station in data['results']:
+            timeseries[station['id']] += timestamp + ', '
+            timeseries[station['id']] += str(station['availableDocks']) + ', '
+            timeseries[station['id']] += str(station['availableBikes']) + '\n'
+
+
+for k, v in timeseries.items():
+    with open('timeseries/' + str(k) + '.csv', 'w') as f:
+        header = 'Timestamp, AvailableBikes, AvailableDocks\n'
+        body = header + v
+        f.write(body)
