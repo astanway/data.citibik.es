ed13f3d1ff8e2bb63a06386a71e106c9e1e4c0c7
Abe Stanway <abestanway@gmail.com>
2013-06-02 07:43:11 +0000

    New scraper for graphite

---
+++ get_json.py
@@ -0,0 +1,14 @@
+import requests
+import time
+
+url = "http://appservices.citibikenyc.com/data2/stations.php"
+
+def data_me_bro():
+  name = str(int(time.time()))
+  with open('/home/abasababa/citibike/data/' + name, 'w') as f:
+    r = requests.get(url)
+    f.write(r.content)
+
+if __name__ == "__main__":
+  data_me_bro()
+
--- scrape.py
+++ scrape.py
@@ -1,14 +1,27 @@
-import requests
+import os
+import simplejson as json
 import time
+import requests
+import socket
 
 url = "http://appservices.citibikenyc.com/data2/stations.php"
+r = requests.get(url)
+data = json.loads(r.content)
 
-def data_me_bro():
-  name = str(int(time.time()))
-  with open('/home/abasababa/citibike/data/' + name, 'w') as f:
-    r = requests.get(url)
-    f.write(r.content)
+def removeNonAscii(s): return "".join(i for i in s if ord(i)<128)
 
-if __name__ == "__main__":
-  data_me_bro()
+CARBON_SERVER = '127.0.0.1'
+CARBON_PORT = 2003
 
+sock = socket.socket()
+sock.connect((CARBON_SERVER, CARBON_PORT))
+
+for station in data['results']:
+    lines = []
+    station['timestamp'] = data['lastUpdate']
+    station['label'] = removeNonAscii(station['label'])
+    station['label'] = station['label'].replace(' ', '-').replace('.', '').replace('\'', '')
+    lines.append('%(label)s.available_bikes %(availableBikes)s %(timestamp)s' % station)
+    lines.append('%(label)s.available_docks %(availableDocks)s %(timestamp)s' % station)
+    message = '\n'.join(lines) + '\n'
+    sock.sendall(message)
