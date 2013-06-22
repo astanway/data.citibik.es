5a54e7e5c158cb13fcb9ea1268e806c6394e68b0
Abe Stanway <abestanway@gmail.com>
2013-06-22 15:15:28 +0000

    Exception and ports

--- scrape.py
+++ scrape.py
@@ -6,15 +6,21 @@ import socket
 
 url = "http://appservices.citibikenyc.com/data2/stations.php"
 r = requests.get(url)
-data = json.loads(r.content)
+try:
+    data = json.loads(r.content)
+except:
+    print r.content
+    raise Exception("No JSON dog")
 
 def removeNonAscii(s): return "".join(i for i in s if ord(i)<128)
 
 CARBON_SERVER = '127.0.0.1'
-CARBON_PORT = 2003
+CARBON_PORT = 2013
 
 sock = socket.socket()
 sock.connect((CARBON_SERVER, CARBON_PORT))
+s = socket.socket()
+s.connect((CARBON_SERVER, 2003))
 
 for station in data['results']:
     lines = []
@@ -25,3 +31,4 @@ for station in data['results']:
     lines.append('%(label)s.available_docks %(availableDocks)s %(timestamp)s' % station)
     message = '\n'.join(lines) + '\n'
     sock.sendall(message)
+    s.sendall(message)
